acf8787da1d4c41a543ead31ea3c152432a61b95
Ameni <ameni.abdelghani@steps.tn>
2023-01-05 16:02:17 +0100

    roter

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAp8KxjNyqDxYo_-2tCXyPAFDsaf48WAVY",
+    authDomain: "fir-a-8c6d8.firebaseapp.com",
+    projectId: "fir-a-8c6d8",
+    storageBucket: "fir-a-8c6d8.appspot.com",
+    messagingSenderId: "248217571123",
+    appId: "1:248217571123:web:dd7c4ddad1e540b9b9b846",
+    measurementId: "G-F0VC2DHDK2"
+  };
